7f709dadcfff14101269a2e2da31c34518aea68c
nexnema <nexnema@gmail.com>
2023-02-26 23:53:16 +0330

    add cards

--- src/routes/intro/categories.tsx
+++ src/routes/intro/categories.tsx
@@ -16,34 +16,33 @@ import MenuLists from "../../assets/json/menu.json";
 
 function Categories() {
     return (
-        <Container maxWidth="lg" style={{ padding: "4rem 2rem" }}>
-            <Row>
+        <div>
+            <AppBar style={{ position: "sticky", top: "0" }}>
                 {MenuLists.categories.map(item => (
                     <Button variant="filled">{item.name}</Button>
                 ))}
-            </Row>
-            {MenuLists.categories.map(item => (
-                <div>
-                    <h2>{item.name}</h2>
-                    <Hr />
-                    <GridContainer>
-                        {item.list.map(item => (
-                            <GridItem
-                                xl={4}
-                                md={6}
-                                sm={12}
-                                style={{
-                                    minHeight: "5rem",
-                                    background: "red",
-                                }}
-                            >
-                                <ListItem data={item} />
-                            </GridItem>
-                        ))}
-                    </GridContainer>
-                </div>
-            ))}
-        </Container>
+            </AppBar>
+            <Container maxWidth="xl" style={{ flexDirection: "column", padding: "4rem 2rem" }}>
+                {MenuLists.categories.map(item => (
+                    <div style={{ padding: "2rem 0" }}>
+                        <h2>{item.name}</h2>
+                        <Hr />
+                        <GridContainer style={{ padding: "2rem" }} rowGap={2} columnGap={2}>
+                            {item.list.map(item => (
+                                <GridItem
+                                    xl={4}
+                                    lg={6}
+                                    md={6}
+                                    sm={12}
+                                >
+                                    <ListItem data={item} />
+                                </GridItem>
+                            ))}
+                        </GridContainer>
+                    </div>
+                ))}
+            </Container>
+        </div>
     );
 }
 
--- src/routes/intro/listItem.tsx
+++ src/routes/intro/listItem.tsx
@@ -1,4 +1,4 @@
-import { Row, Spacer, Stack } from "@yakad/ui";
+import { Row, Spacer, Stack, Card, } from "@yakad/ui";
 import images from "../../assets/images/index";
 
 interface ListItems {
@@ -13,23 +13,28 @@ interface ListProps extends React.HTMLAttributes<HTMLElement> {
 }
 function ListItem(props: ListProps) {
     return (
-        <div {...props}>
+        <Card {...props} style={{ padding: "0", backgroundColor: "#222", overflow: "hidden" }}>
             {" "}
             <Row style={{ direction: "rtl" }}>
                 <img
-                    width="30%"
                     src={
-                        props.data.image ? images[props.data.image] : images[1]
+                        props.data.image ? images[props.data.image] : images[0]
                     }
+                    style={{
+                        width: "15rem",
+                        height: "15rem"
+                    }}
                 />
-                <Stack>
-                    <h3>{props.data.name}</h3>
-                    <p>{props.data.dicribtion}</p>
+                <Stack style={{ height: "13rem", margin: "1rem 0" }}>
+                    <h3 style={{ margin: "0", fontSize: "1.6rem" }}>{props.data.name}</h3>
+                    <p style={{ fontSize: "1.3rem" }}>{props.data.dicribtion}</p>
+                    <div>
+                        <span style={{ fontSize: "2.2rem" }}>{props.data.price}{props.data.price_double ? " - " + props.data.price_double : null}</span>
+                        <span style={{ fontSize: "1.2rem" }}>  هزار تومان</span>
+                    </div>
                 </Stack>
-                <Spacer />
-                <div>{props.data.price}</div>
             </Row>
-        </div>
+        </Card>
     );
 }
 
