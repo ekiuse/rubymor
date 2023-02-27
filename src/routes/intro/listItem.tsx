2016dbef41f7a40d4d1e926674e57495cd16b6fc
erfanekrami <erfanak964@gmail.com>
2023-02-27 17:45:26 +0330

    ADD Font

--- src/routes/intro/listItem.tsx
+++ src/routes/intro/listItem.tsx
@@ -1,4 +1,4 @@
-import { Row, Spacer, Stack, Card, } from "@yakad/ui";
+import { Row, Spacer, Stack, Card } from "@yakad/ui";
 import images from "../../assets/images/index";
 
 interface ListItems {
@@ -13,7 +13,14 @@ interface ListProps extends React.HTMLAttributes<HTMLElement> {
 }
 function ListItem(props: ListProps) {
     return (
-        <Card {...props} style={{ padding: "0", backgroundColor: "#222", overflow: "hidden" }}>
+        <Card
+            {...props}
+            style={{
+                padding: "0",
+                backgroundColor: "#222",
+                overflow: "hidden",
+            }}
+        >
             <Row style={{ direction: "rtl" }}>
                 <img
                     src={
@@ -21,15 +28,24 @@ function ListItem(props: ListProps) {
                     }
                     style={{
                         width: "15rem",
-                        height: "15rem"
+                        height: "15rem",
                     }}
                 />
                 <Stack style={{ height: "13rem", margin: "1rem 0" }}>
-                    <h3 style={{ margin: "0", fontSize: "1.6rem" }}>{props.data.name}</h3>
-                    <p style={{ fontSize: "1.3rem" }}>{props.data.dicribtion}</p>
+                    <h3 style={{ margin: "0", fontSize: "1.6rem" }}>
+                        {props.data.name}
+                    </h3>
+                    <p style={{ fontSize: "1.3rem" }}>
+                        {props.data.dicribtion}
+                    </p>
                     <div>
-                        <span style={{ fontSize: "2.2rem" }}>{props.data.price}{props.data.price_double ? " - " + props.data.price_double : null}</span>
-                        <span style={{ fontSize: "1.2rem" }}>  هزار تومان</span>
+                        <span style={{ fontSize: "2.2rem" }}>
+                            {props.data.price}
+                            {props.data.price_double
+                                ? " - " + props.data.price_double
+                                : null}
+                        </span>
+                        <span style={{ fontSize: "1.2rem" }}> هزار تومان</span>
                     </div>
                 </Stack>
             </Row>
